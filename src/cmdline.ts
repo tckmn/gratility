112261ab6c05c3c5e771d979bd2eb5ad05770243
Andy Tockman <andy@tck.mn>
2025-04-10 15:26:45 -0400

    more command line functionality

--- src/cmdline.ts
+++ src/cmdline.ts
@@ -5,9 +5,30 @@ import Image from './image.js';
 
 import { JSDOM } from 'jsdom';
 import fs from 'node:fs';
+import { parseArgs } from 'node:util';
 
-const imgpad = 1;
-const gridpad = 0;
+
+const { values, positionals } = parseArgs({
+    options: {
+        script: { type: 'string', short: 's' },
+        out: { type: 'string', short: 'o' },
+        help: { type: 'boolean', short: 'h' }
+    },
+    allowPositionals: true
+});
+
+if (values.help || positionals.length !== 1) {
+    console.log(`usage: ${process.argv[1]} [options] INPUT`);
+    console.log('INPUT can be:');
+    console.log('  a filename (containing binary stamp data)');
+    console.log('  a base64-encoded string');
+    console.log('options:');
+    console.log('  --help|-h          show this message');
+    console.log('  --out|-o FILE      specify output filename (derived from input by default)');
+    console.log('  --script|-s FILE   run script on stamp and output new stamp');
+    process.exit();
+}
+
 
 function interpret(s: string) {
     try {
@@ -17,13 +38,30 @@ function interpret(s: string) {
     }
 }
 
+function outname(s: string) {
+    if (values.out) return values.out;
+    const ext = values.script ? '.new.stamp' : '.svg';
+    if (s.slice(-6) === '.stamp') return s.replace(/\.stamp$/, ext);
+    return 'out'+ext;
+}
+
+
+const imgpad = 1;
+const gridpad = 0;
+
 const dom = new JSDOM('<svg id="grid"></svg>');
 const svg = dom.window.document.getElementById('grid') as unknown as SVGElement;
 const image = new Image(dom.window.document, svg);
 Data.initialize(image);
 
-const arg = process.argv[2];
-const stamp = interpret(arg);
+const stamp = interpret(positionals[0]);
+if (values.script) {
+    // TODO check if outname exists before running script
+    // TODO also add a flag -f probably that overwrites
+    require(values.script).runScript(stamp, { Data, Stamp, Measure });
+    fs.writeFileSync(outname(positionals[0]), Data.serialize(stamp.cells), { flag: 'wx' });
+    process.exit();
+}
 stamp.apply(0, 0);
 
 const xmin = Math.floor(stamp.xmin/2)*2;
@@ -35,4 +73,4 @@ image.grid(xmin-gridpad, xmax+gridpad, ymin-gridpad, ymax+gridpad);
 image.text.setAttribute('transform', 'translate(0 2.5)');
 svg.setAttribute('viewBox', `${Measure.HALFCELL*(xmin-imgpad)} ${Measure.HALFCELL*(ymin-imgpad)} ${Measure.HALFCELL*(xmax-xmin+2*imgpad)} ${Measure.HALFCELL*(ymax-ymin+2*imgpad)}`);
 
-fs.writeFileSync(arg.slice(-6) === '.stamp' ? arg.replace(/\.stamp$/, '.svg') : 'out.svg', svg.outerHTML);
+fs.writeFileSync(outname(positionals[0]), svg.outerHTML, { flag: 'wx' });
